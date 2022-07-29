bfedae6e29a2248035c09f81695ae850e40bc2ba
Katerina <sanmartinkaterina@gmail.com>
2022-07-29 15:06:36 -0300

    models-db

---
+++ api/src/models/Temperament.js
@@ -0,0 +1,17 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('temperament', {
+        id: {
+         type: DataTypes.UUID,
+         defaultValue: DataTypes.UUIDV4,
+         allowNull: true,
+         primaryKey: true  
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            unique: true
+        }
+    },{timestamps: false});
+};
